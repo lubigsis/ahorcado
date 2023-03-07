4185a38ec9955bb04c2f1c5e5b7529d8325646ac
lubigsis <lucre.baluc@gmail.com>
2023-03-06 22:53:20 -0300

    Primer commit: HTML y CSS, muy poco de JavaScript

---
+++ main.js
@@ -0,0 +1,10 @@
+//----------------------------------------------------------defino variables----------------------------
+let palabras = ['elefante', 'biblioteca', 'camioneta', 'acordeon', 'relampago'];
+
+let palabra = palabras[Math.floor(Math.random() * palabras.length)]; //redondeo y saco palabra al azar
+
+
+
+document.querySelector('#calcular').addEventListener('click', () => {
+    
+})

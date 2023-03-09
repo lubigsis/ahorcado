a506f381b597da8610e2c626daebdecfdc6ab500
lubigsis <lucre.baluc@gmail.com>
2023-03-08 22:48:11 -0300

    Muestro guiones de la palabra a adivinar en la pantalla y subo img del ahorcado, como si fuese un slider.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 //----------------------------------------------------------defino variables----------------------------
 let palabras = ['elefante', 'biblioteca', 'camioneta', 'acordeon', 'relampago'];
-
 let palabra = palabras[Math.floor(Math.random() * palabras.length)]; //redondeo y saco palabra al azar
-
 let palabraConGuiones = palabra.replace(/./g, "_ "); //para transformar en guines las palabras
+let contadorFallos = 0;
+
 
 //---------------------------------------------------------------------------------------------------
-
 
 
 String.prototype.replaceAt=function(index, character) { return this.substring(0, index) + character + this.substring(index+character.length); } 
@@ -17,17 +16,25 @@ String.prototype.replaceAt=function(index, character) { return this.substring(0,
 
 //---------------------------------------------------------------------------------------------------
 
-
 document.querySelector('#calcular').addEventListener('click', () => {
         
     let letra = document.querySelector('#letra').value;
-    alert(palabra);
+    let fallaste = true;
     //-----------------------------reviso si hay coincidencias
     for(let i in palabra){
         if(letra == palabra[i]){
             palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra); //donde encunetra la coincidencia, pone la letra
+            fallaste = false;
         }
     }
-    
-    alert(palabraConGuiones);
+
+        if(fallaste){
+            contadorFallos++;
+            document.querySelector('#ahorcado').style.backgroundPosition = - (307 * contadorFallos) + 'px 0'; //como el slider
+        }
+
+
+
+    document.querySelector('#output').innerHTML = palabraConGuiones;
+    //alert(palabraConGuiones);
 });
